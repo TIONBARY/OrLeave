735d212da3c2488a0a63d36ff753cfb8af890999
SSAFY <dddalang4@naver.com>
2022-07-28 15:21:44 +0900

    Modify: Repository 수정

--- backend/src/main/java/com/orleave/repository/MeetingSettingRepository.java
+++ backend/src/main/java/com/orleave/repository/MeetingSettingRepository.java
@@ -1,9 +1,11 @@
 package com.orleave.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.orleave.entity.MeetingSetting;
 
+@Repository
 public interface MeetingSettingRepository extends JpaRepository<MeetingSetting, Long> {
 
 }
