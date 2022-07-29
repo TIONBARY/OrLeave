b97a6aa3be45df4d34a42c049954657fa2afde33
SSAFY <dddalang4@naver.com>
2022-07-29 16:21:14 +0900

    Feat: 인증메일 전송 기능 추가

---
+++ backend/src/main/java/com/orleave/config/EmailConfig.java
@@ -0,0 +1,59 @@
+package com.orleave.config;
+
+import java.util.Properties;
+ 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+ 
+ 
+@Configuration
+@PropertySource("classpath:email.properties")
+public class EmailConfig {
+ 
+    @Value("${mail.smtp.port}")
+    private int port;
+    @Value("${mail.smtp.socketFactory.port}")
+    private int socketPort;
+    @Value("${mail.smtp.auth}")
+    private boolean auth;
+    @Value("${mail.smtp.starttls.enable}")
+    private boolean starttls;
+    @Value("${mail.smtp.starttls.required}")
+    private boolean startlls_required;
+    @Value("${mail.smtp.socketFactory.fallback}")
+    private boolean fallback;
+    @Value("${AdminMail.id}")
+    private String id;
+    @Value("${AdminMail.password}")
+    private String password;
+    
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.naver.com");
+        javaMailSender.setUsername(id);
+        javaMailSender.setPassword(password);
+
+        javaMailSender.setPort(port);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        return properties;
+    }
+}
--- backend/src/main/java/com/orleave/controller/UserController.java
+++ backend/src/main/java/com/orleave/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orleave.auth.SsafyUserDetails;
@@ -15,6 +16,7 @@ import com.orleave.dto.request.SignupRequestDto;
 import com.orleave.dto.response.BaseResponseDto;
 import com.orleave.dto.response.UserResponseDto;
 import com.orleave.entity.User;
+import com.orleave.service.EmailService;
 import com.orleave.service.UserService;
 
 import io.swagger.annotations.Api;
@@ -34,6 +36,9 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	EmailService mailService;
 	
 	@PostMapping("/signup")
 	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
@@ -71,4 +76,20 @@ public class UserController {
 		
 		return ResponseEntity.status(200).body(UserResponseDto.of(user));
 	}
+	
+	@PostMapping("/emailConfirm")
+    @ApiOperation(value = "회원 가입시 이메일 인증", notes = "기존 사용하고 있는 이메일을 통해 인증")
+	@ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+        @ApiResponse(code = 401, message = "인증 실패"),
+        @ApiResponse(code = 404, message = "사용자 없음"),
+        @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BaseResponseDto> emailConfirm(
+            @RequestParam @ApiParam(value="이메일정보 정보", required = true) String email) throws Exception {
+ 
+        String confirm = mailService.sendSimpleMessage(email);
+ 
+        return ResponseEntity.status(200).body(new BaseResponseDto(200, confirm));
+    }
 }
