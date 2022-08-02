d05c8e277fe2d85be7df70d4ac18fa6cfb2324d0
SSAFY <dddalang4@naver.com>
2022-08-02 14:38:17 +0900

    Modify: Notice Entity 수정

--- backend/src/main/java/com/orleave/entity/Notice.java
+++ backend/src/main/java/com/orleave/entity/Notice.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -24,7 +26,7 @@ import lombok.Setter;
 @Builder
 public class Notice {
 	@Id
-	@Column(name = "no", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int no;
 	
 	@Column(name = "title", nullable = false, length = 100)
