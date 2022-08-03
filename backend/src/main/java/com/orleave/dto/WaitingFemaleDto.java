371d1d0841023e6f6dfeaeffe903cd9a2140cbe8
SSAFY <dddalang4@naver.com>
2022-08-03 22:23:56 +0900

    Feat: 매칭 대기 사용자 정보 DTO 생성

---
+++ backend/src/main/java/com/orleave/dto/WaitingFemaleDto.java
@@ -0,0 +1,36 @@
+package com.orleave.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ApiModel("NoticeListDto")
+public class WaitingFemaleDto {
+	@ApiModelProperty(name="사용자 번호")
+	int no;
+	@ApiModelProperty(name="사용자 나이")
+	int age;
+	@ApiModelProperty(name="사용자 현재 위치 위도")
+	double lat;
+	@ApiModelProperty(name="사용자 현재 위치 경도")
+	double lng;
+	@ApiModelProperty(name="사용자 닉네임")
+	String nickname;
+	@ApiModelProperty(name="사용자 MBTI", example="ISFJ")
+	String mbti;
+	@ApiModelProperty(name="사용자 종교", example="1")
+	int religion;
+	@ApiModelProperty(name="사용자 흡연 여부", example="0")
+	int smoke;
+	@ApiModelProperty(name="사용자 음주 여부", example="0")
+	int drink;
+	@ApiModelProperty(name="사용자 프로필 이미지 번호", example="10")
+	int imageNo;
+}
