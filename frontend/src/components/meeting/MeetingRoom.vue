bf2afcfcb8c810bbb419e29c73018b4afa3f5574
정지은 <lisajieun@naver.com>
2022-08-04 17:57:01 +0900

    Design: 미팅룸 추가

--- frontend/src/components/meeting/MeetingRoom.vue
+++ frontend/src/components/meeting/MeetingRoom.vue
@@ -1,9 +1,31 @@
 <template>
   <div>
-    r o o m
+    <h2>1단계</h2>
     <div class="row q-gutter-md justify-center">
-      <q-skeleton width="500px" height="375px" />
-      <q-skeleton width="500px" height="375px" />
+      <div>
+        <q-skeleton width="500px" height="375px" />
+        <div class="q-ma-sm">
+          <q-img src="../../assets/profile/13.png" width="70px" />
+          <span class="q-ml-sm" style="font-size: 24px">ooo님</span>
+        </div>
+      </div>
+      <div>
+        <q-skeleton width="500px" height="375px" />
+        <div class="q-ma-sm">
+          <q-img src="../../assets/profile/23.png" width="70px" />
+          <span class="q-ml-sm" style="font-size: 24px">ㅁㅁㅁ님</span>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-between">
+      <q-btn flat color="warning" to="/">
+        <q-img src="../../assets/meeting/skip.png" width="130px" />
+      </q-btn>
+
+      <q-btn flat color="primary" to="/">
+        <q-img src="../../assets/meeting/leave.png" width="130px" no-spinner />
+      </q-btn>
     </div>
   </div>
 </template>
+<style scoped></style>
