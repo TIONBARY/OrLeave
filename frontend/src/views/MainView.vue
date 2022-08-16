56e3c779e25a6caa84851e932a73a4ec7dd65a9e
rlatldjs123 <rlatldjs123@gmail.com>
2022-08-16 23:15:24 +0900

    Design: 메인화면 조정

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -2,7 +2,6 @@
   <div>
     <menu-bar />
     <main-content />
-    <!-- <main-index /> -->
   </div>
 </template>
 
@@ -10,12 +9,10 @@
 // @ is an alias to /src
 import MainContent from '@/components/MainContent.vue'
 import MenuBar from '@/components/MenuBar.vue'
-// import MainIndex from '@/components/MainIndex.vue'
 export default {
   components: {
     MainContent,
     MenuBar
-    // MainIndex
   }
 }
 </script>
